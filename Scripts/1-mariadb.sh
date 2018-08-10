9db7b0e14ad4d3568b0b9ff07bfeab05c724da29
Farsos Bulsara <kingsukhoi@gmail.com>
2018-08-10 16:41:22 -0600

    database backups work now

--- Scripts/1-mariadb.sh
+++ Scripts/1-mariadb.sh
@@ -1,1 +1,1 @@
-mysqldump -u root -p 'cell00highway'  | restic $REPO_LOC backup --stdin
+mysqldump --all-databases -u root -p'cell00highway'  | restic backup --tag db --stdin --stdin-filename mariadb.sql
