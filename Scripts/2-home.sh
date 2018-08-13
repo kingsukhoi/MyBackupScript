11b569c84173b5f41c297342d5c0ddd5f2bd6f8e
Farsos Bulsara <kingsukhoi@gmail.com>
2018-08-13 12:56:28 -0600

    these are now deleted didn't do it before apparently

--- Scripts/2-home.sh
+++
@@ -1,1 +0,0 @@
-restic backup --exclude-caches --exclude /home/farsos/.cache/ --exclude *.iso --exclude /home/farsos/Music --exclude /home/farsos/videos --exclude /home/farsos/.local/share/Trash/ /home/
