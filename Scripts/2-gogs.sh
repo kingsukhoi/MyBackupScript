27b1ad081a0c88acfc484cf161a78447cc8ab764
Farsos Bulsara <kingsukhoi@gmail.com>
2018-08-13 10:49:09 -0600

    changed to backup gogs config data dir instead of previous setup

--- Scripts/2-gogs.sh
+++ Scripts/2-gogs.sh
@@ -1,7 +1,9 @@
-backup_file_dir='/tmp/gogsbackup' 
-su git -c "mkdir $backup_file_dir && /opt/gogs/gogs backup --target $backup_file_dir/"
-unzip -d "$backup_file_dir/" "$backup_file_dir/*"
-unzip -d "$backup_file_dir/gogs-backup/repositories" "$backup_file_dir/gogs-backup/repositories.zip"
-rm -f "$backup_file_dir/gogs-backup/repositories.zip"
-restic backup --tag app --tag conf --tag gogs "$backup_file_dir/gogs-backup"
-rm -rf "$backup_file_dir/"
+#backup_file_dir='/tmp/gogsbackup' 
+#su git -c "mkdir $backup_file_dir && /opt/gogs/gogs backup --target $backup_file_dir/"
+#unzip -d "$backup_file_dir/" "$backup_file_dir/*"
+#unzip -d "$backup_file_dir/gogs-backup/repositories" "$backup_file_dir/gogs-backup/repositories.zip"
+#rm -f "$backup_file_dir/gogs-backup/repositories.zip"
+#restic backup --tag app --tag conf --tag gogs "$backup_file_dir/gogs-backup"
+#rm -rf "$backup_file_dir/"
+restic backup --tag conf --tag gogs /opt/gogs/custom/conf /opt/gogs/data
+restic backup --tag gogs --tag app /var/gogs
